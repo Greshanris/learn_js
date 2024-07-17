dd0d671be2885faf6138494b53b5b18d41b94e91
Rishav Chaudhary <124981063+Greshanris@users.noreply.github.com>
2024-07-17 13:29:29 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer version
+
+// alert (3+3) we are using node.js, not browser
+
+console.log( 3 + 3 ) // code readability should be high
+
+console.log("Rishav")
+
+let age = 23;
+let isLoggedIn = false;
+let temp = null;
+let state;
+
+// documentation: mdn => mozilla, another: ecma 
+
+// number => 2 to power 53
+// bigint => trading, stock market, facebook
+// string => ""
+// boolean => true/false
+// null => standalone value: empty
+// undefined => no value defined 
+// symbol : to identify unique componet, could be understand in react, figma, when there are too many components.
+
+// object
+
+console.log(typeof null); // datatype: object
+console.log(typeof undefined); // datatype: undefined
