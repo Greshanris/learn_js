8167b33c9ce032d1bd3b64e06c75bfe2ca122633
Rishav Chaudhary <124981063+Greshanris@users.noreply.github.com>
2024-07-17 12:51:54 +0000

    variable file added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553;
+let accountEmail = "rishavc957@gmail.com";
+var accountPassword = "12345";
+accountCity = "Jaipur";
+let accountState;
+// accountId = 2 // not allowed
+
+accountEmail = "hc@hc.com";
+accountPassword = "233333";
+accountCity = "Bengaluru";
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
