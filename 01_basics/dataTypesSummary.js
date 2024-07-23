b7d97fe266abace3dfa9f8215dfb2e8c05e39c56
Rishav Chaudhary <124981063+Greshanris@users.noreply.github.com>
2024-07-23 12:15:00 +0000

    updated datatypes summary

--- 01_basics/dataTypesSummary.js
+++ 01_basics/dataTypesSummary.js
@@ -31,20 +31,46 @@ console.log(id === anotherId); // symbol makes same value unique as their data t
 
 // Array, Objects, Functions
 
-const heros = ["shaktiman", "naagraj", "doga"] // arrays
+// const heros = ["shaktiman", "naagraj", "doga"] // arrays
 
-let myObj = {
-    name: "rishav",
-    age: 22,
-} // objects, everything inside the curly braces
+// let myObj = {
+//     name: "rishav",
+//     age: 22,
+// } // objects, everything inside the curly braces
 
-const myFunction = function(){
-    console.log("Hello World!")
-}
+// const myFunction = function(){
+//     console.log("Hello World!")
+// }
 
-console.log(typeof myObj); 
+// console.log(typeof myObj); 
 
 // heros (array): datatypes: object
 // myObj (object): datatypes: object
 // myFunction (function): datatypes: function (object function)
 
+// ++++++++++++++++++++++++
+// stack (Primitive)- memory copy of the variable, heap (Non-primitive)- actual reference of data, meaning changing the original value changes the value.
+
+let myYoutubename = "rishavchaudharydotcom" // original value
+
+let anothername = myYoutubename // a copy of original value is given to the variable (memory reference)
+anothername = "chaiaurrishav" // when changed, the copy value get's changed in copy memory reference
+
+/* It can be described like stacking of memory value in a particular memory address. When the first change happens, then the copy value is made in the upper stack. When again the value is changed, the copy value gets stacked again. */
+
+console.log(anothername); // chaiaurrishav
+console.log(myYoutubename); // rishavchaudhary.com
+
+// This is the part of the object which is heap giving actual memory reference.
+
+let userOne ={
+    email: "user@google.com",
+    upi: "user@ybl"
+} 
+
+let userTwo = userOne // giving the reference of actual data not copy
+
+userTwo.email = "hitesh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
