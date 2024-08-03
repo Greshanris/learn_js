d669a3e11fd40526b98b832a73749b4057a11f1e
Rishav Chaudhary <124981063+Greshanris@users.noreply.github.com>
2024-08-03 14:20:42 +0000

    04_objects.js updated

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,9 +1,28 @@
 // object singleton: constructor part
 
-const tinderUser = new Object()
+// const tinderUser = new Object() // this is singleton object which is constructed
 
-console.log(tinderUser); //{}
+// console.log(tinderUser); //{}
 
-const newtinderUser = {}
+// const newtinderUser = {} // this is not singleton object
 
-console.log(newtinderUser); //{}
+// console.log(newtinderUser); //{}
+
+const tinderUser = {}
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname : {
+        userFullName: {
+            firstName: "Rishav",
+            lastName: "chaudhary"
+        }
+    }
+}
+
+console.log(regularUser.fullname?.userFullName.firstName);
