b11c8c92695ae7298ddae8844168779d62bb6ad1
Rishav Chaudhary <124981063+Greshanris@users.noreply.github.com>
2024-08-07 12:45:53 +0000

    updated 04_objects.js

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -25,6 +25,63 @@ const regularUser = {
     }
 }
 
-console.log(regularUser.fullname.userFullName.firstName); // This is one way to access objects inside objects
+// console.log(regularUser.fullname.userFullName.firstName); // This is one way to access objects inside objects
 // optional chaining
-// 
+// if there is no fullname then we can use it in this way
+// console.log(regularUser.fullname?.userFullName.firstName); // we use this syntax, when there is response from API
+
+
+// object combination
+const obj1 = {
+    1: "a",
+    2: "b"
+}
+const obj2 = {
+    3: "a",
+    4: "b"
+}
+const obj4 = {
+    5: "a",
+    6: "b"
+}
+
+// const obj3 = {obj1, obj2}
+// console.log(obj3); // { obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'a', '4': 'b' } }
+
+// const obj3 = Object.assign({}, obj1, obj2, obj4) 
+/*Copy the values of all of the 
+enumerable own properties from 
+one or more source objects to a 
+target object. Returns the 
+target object. */
+// {}: optional parameter, guaranteed result(target, source)
+
+const obj3 = {...obj2, ...obj2, ...obj4} // spread operator
+
+// console.log(obj3); // { '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
+
+// objects coming from databases are arrays of objects
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+]
+
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser)); // data type is in array, now we can use it in loops
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser)); // [ [ 'id', '123abc' ], [ 'name', 'Sammy' ], [ 'isLoggedIn', false ] ]
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn')); // true
+
