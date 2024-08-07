996b638517632632c76ffb4f8eb018d5d1e4f00b
Rishav Chaudhary <124981063+Greshanris@users.noreply.github.com>
2024-08-07 14:42:22 +0545

    Update 05_day3exercise.js

--- 00_exercises/05_day3exercise.js
+++ 00_exercises/05_day3exercise.js
@@ -10,10 +10,25 @@ if (isNaN(base) || isNaN(height) || base <= 0 || height <= 0) {
     console.log("Please enter valid positive numbers for base and height.")
 } else {
     const area = 0.5 * base * height;
-    console.log(`The area of the triangle is ${area}`);
+console.log(`The area of the triangle is ${area}`);
 }
 
 // 2. Write a script that prompt the user to enter side a, side b, and side c of the triangle and and calculate the perimeter of triangle (perimeter = a + b + c)
 const sideA = parseFloat(prompt("Enter side a: "))
 const sideB = parseFloat(prompt("Enter side b: "))
-const sideC = parseFloat(prompt(""))
+const sideC = parseFloat(prompt("Enter side c: "))
+
+function perimeter (A, B, C) {
+    let result;5
+    
+    if (isNaN(A) || isNaN(B) || isNaN(C)) {
+        result = "NaN"
+    } else if ((A < 0) || (B < 0) || (C < 0)) {
+        result = "negative"
+    } else {
+        result = A + B + C
+    }
+
+    return `The perimeter is ${result}`
+}
+console.log(perimeter(sideA, sideB, sideC))
