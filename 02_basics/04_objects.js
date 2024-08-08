9f20492dba72167093da54b2c544ba86a6793cb3
Rishav Chaudhary <124981063+Greshanris@users.noreply.github.com>
2024-08-08 11:19:24 +0000

    destructuring object and updating json

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -77,11 +77,60 @@ const users = [
 ]
 
 users[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser)); // data type is in array, now we can use it in loops
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser)); // [ [ 'id', '123abc' ], [ 'name', 'Sammy' ], [ 'isLoggedIn', false ] ]
+// console.log(Object.keys(tinderUser)); // data type is in array, now we can use it in loops
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser)); // [ [ 'id', '123abc' ], [ 'name', 'Sammy' ], [ 'isLoggedIn', false ] ]
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn')); // true
+// console.log(tinderUser.hasOwnProperty('isLoggedIn')); // true
 
+// Destructuring of Objects and arrays:
+// Destructuring of objects
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+// we can use 
+// course.courseInstructor to know the value or logging it in console
+// we can also use course["courseInstructor"]
+
+// but we can't always write this repetedly, if we have to log it three or more times to the console
+// The readability of the code is also something to be concerned about
+// For clean code, we use this syntax
+const {courseInstructor: instructor} = course // const {value's key to be extracted} = objectname
+// there is slight change here, because we can give its own name and access it using that name
+console.log(instructor); // now we can directly use the key to access the value
+// this is what destructuring of objects means
+// console.log(course); // { coursename: 'js in hindi', price: '999', courseInstructor: 'hitesh' } // see the keyname is not changed
+
+// we use these in react: 
+// There are methods in react such as 
+// const navbar = ({company}) => {
+
+// }
+// navbar(company = "hitesh")
+// // in react, by default there are props, we don't use props.company, props.something
+// // we use concept of {company} in params of the function: here destructuring object is used
+// console.log(navbar);
+
+ // ++++++++++++++++++++++++++
+
+// API concepts:
+// API: giving our work to others, its not our headache if the by clicking login button, how a google will verify it.
+// API: middleman
+// API: when we get values from backend, it is how we write it:
+// previously, the file came in xml structure which was complex.
+// now, it comes in json structure
+
+// json looks just like object without name(key: value pair)
+
+// later on we hold in variable
+// proper json structure: key is also in string, and value is also in string
+// {
+//     "name": "rishav",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
