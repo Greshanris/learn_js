007a273eb9c3d3d20f7731f197c2a3fbf0a852cb
Rishav Chaudhary <124981063+Greshanris@users.noreply.github.com>
2024-08-09 09:58:15 +0000

    update 01_functions.js

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -33,3 +33,40 @@ function loginUserMessage(username = "sam") {
 
 // console.log(loginUserMessage("rishav"))
 // console.log(loginUserMessage("rishav")) // undefined
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+// console.log(calculateCartPrice(200, 400, 500, 2000)) // if two or more item is added in arguments, when only one param is stated
+// This quite common if we are buying many items with prices: only 
+// To solve this, there is Rest Operators ... 
+// Rest: the items are there, they are open distributor, just pack it in one and give it to me
+// this will give array [ 200, 400, 500 ]
+// val1, val2, ...num1 is used then first two are in val1 and val2, while rest are in num1
+// ... is both rest operator and spread operator, the difference lies in its use
+
+// how to pass object in function and how to use it
+const user = {
+    username: "rishav",
+    prices: 199
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+    
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,  400, 500, 1000]));
