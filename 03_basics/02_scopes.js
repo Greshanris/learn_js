853d6a8d832a93fc3b708984b0bd00c4f6fa2e5b
Rishav Chaudhary <124981063+Greshanris@users.noreply.github.com>
2024-08-10 14:34:28 +0000

    updated 02_scopes.js

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -12,13 +12,47 @@ let a = 300
 if (true) {
     let a = 10
     const b = 20
-    console.log("INNER: ", a);
-    
+    // console.log("INNER: ", a); 
 } // All the thing that is written inside the if {} is block scope
 
-
-
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one(){
+    const username = "rishav"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    
+    two()
+}
+
+// one()
+
+if (true) {
+    const username = "rishav"
+    if (username === "rishav") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+// +++++++++++++ interesting +++++++++++++++++
+
+console.log(addOne(5))
+function addOne(num) {
+    return num + 1
+}
+
+console.log(addTwo(5))
+const addTwo = function(num){
+    return num + 2
+}
 
