1fb2aed0a418850d1812c2aa250264620e84443f
Rishav Chaudhary <rishavc957@gmail.com>
2024-09-04 14:02:26 +0000

    updated 03_three.js

---
+++ 05_iterations/03_three.js
@@ -0,0 +1,53 @@
+// for of
+
+// Array specific loops
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+const arr = [1, 2, 3, 4, 5]
+// inside here object is different compared to javascript object.
+// syntax: for (const variablename of objectname){}
+for (const num of arr) {
+    // console.log(num);
+    
+}
+
+const greetings = "Hello world!"
+for (const greet of greetings) {
+    if (greet === " ") {
+        continue
+    }
+    // console.log(`Each char is ${greet}`);
+}
+
+// Maps
+
+/* Map is an object which hold key-value pairs and remembers the original insertion order of the keys.
+- no duplicate values */
+
+const map = new Map()
+map.set('IN', "India")
+map.set('Fr', "France")
+map.set('USA', "United States of America")
+map.set('IN', "India")
+
+//console.log(map);
+
+for (const key of map) {
+    console.log(key);
+    
+}
+/* ouputs: [ 'IN', 'India' ]
+[ 'Fr', 'France' ]
+[ 'USA', 'United States of America' ] */ // This is in array format
+
+// But here, if destructure the array like this:
+for (const [key, value] of map) {
+    console.log(key);
+    
+}
+/*
+ IN
+Fr
+USA */
